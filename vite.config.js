730021ceda6f0455d3f1a20171ad3c98600fbd30
thecoolwater <fdemo35@gmail.com>
2026-07-06 15:30:09 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',
   plugins: [react()],
-  build: {
-    cssCodeSplit: true,
-    sourcemap: false,
-  },
-});
+  base: '/TeaNest/',
+})
